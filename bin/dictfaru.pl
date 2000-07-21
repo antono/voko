5eeb5f56aa859529fb82f5da0c914a4b135bf493
revo <revo@5b0f4965-bd4a-0410-a764-d5e1455f809a>
2000-07-21 21:00:18 +0000

    nun kreas ankau alilingvajn indeksojn, ne jam tute preta...
    git-svn-id: https://retavortaro.svn.sourceforge.net/svnroot/retavortaro/trunk@90 5b0f4965-bd4a-0410-a764-d5e1455f809a

--- bin/dictfaru.pl
+++ bin/dictfaru.pl
@@ -1,80 +1,139 @@
 #!/usr/bin/perl
 
+# uzo:
+#   dictfaru.pl -2 <xml-dosierujo>
+#
+
 # farenda:
 #
 #   indeksoj de nacilingvoj, chiuj derivajhoj - ne nur kapvortoj
 #   por tiu havu liston de la dosiernomoj kaj ekstraktu la
 #   referencinformojn per XSL au simile el indekso.xml
 #
-#   traktu chiujn dosierojn de -ujo ne nur elektitajn
-#
 #
 
 
 $verbose = 1;
+$nur_indeksoj = 1; # por pli facila testado
 
 $xslbin = "/home/revo/voko/bin/xslt.sh";
 $xsl = "/home/revo/voko/xsl/revotxt.xsl";
 $tmp = "/home/revo/tmp";
-$datfile = "/home/revo/dict/revo.dat";
-$inxfile = "/home/revo/dict/revo.inx";
+$datfile = "/home/revo/tmp/revo.dat";
+$inxpref = "/home/revo/tmp/revo";
+$indekso = "/home/revo/revo/sgm/indekso.xml~";
 
 $b64 = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
 
-#########
+######### 1a pasho: chiujn XML-artikolojn skribu en grandan tekstdosieron
+#########           kaj skribu la pozicion kaj longon de la artikoloj en
+#########           indeksdosieron
 
 $pos = 0;
 unlink "$datfile";
 
-opendir DIR, shift @ARGV;
+unless ($nur_indeksoj) {
 
-while ($file = readdir DIR) {
+    $dir = shift @ARGV;
+    opendir DIR, $dir;
+    open INX,">$inxpref.inx" or die "Ne eblis malfermi \"$inxpref.inx\" por skribi\n";
 
-    if (-f $file and $file =~ /\.xml$/) {
-
-	print "$file: " if ($verbose);
+    while ($file = readdir DIR) {
 	
-	`$xslbin $file $xsl > $tmp/$file.html`;
-	`lynx -dump $tmp/$file.html >> $datfile`;
-	$len = (-s "$datfile") - $pos;
+	if (-f "$dir/$file" and $file =~ /\.xml$/) {
+	    
+	    print "$file: " if ($verbose);
+
+	    # konvertu XML->TXT kaj alpendigu al datumdosiero
+	    `$xslbin $dir/$file $xsl > $tmp/$file.html`;
+	    `lynx -dump $tmp/$file.html >> $datfile`;
+	    $len = (-s "$datfile") - $pos;
 	
 	# elprenu la kapvorton
-	open HTML, "$tmp/$file.html" 
-	    or die "Ne povis malfermi $tmp/$file.html: $!\n";
-	$html = join('',<HTML>);
-	close HTML;
-	$html =~ /<title>(.*?)<\/title>/si;
-	$kapv = $1;
-	$kapv =~ s/^\s+//s;
-	$kapv =~ s/\s+$//s;
-	$kapv =~ s/\s+/ /sg;
-	$kapv =~ s/\///g;
-	$kapv =~ s/&#(\d+);/int_utf8($1)/eg;
+#	open HTML, "$tmp/$file.html" 
+#	    or die "Ne povis malfermi $tmp/$file.html: $!\n";
+#	$html = join('',<HTML>);
+#	close HTML;
+#	$html =~ /<title>(.*?)<\/title>/si;
+#	$kapv = $1;
+#	$kapv =~ s/^\s+//s;
+#	$kapv =~ s/\s+$//s;
+#	$kapv =~ s/\s+/ /sg;
+#	$kapv =~ s/\///g;
+#	$kapv =~ s/&#(\d+);/int_utf8($1)/eg;
 
-	unlink "$tmp/$file.html";
+	    unlink "$tmp/$file.html";
 	
-	print "[$kapv\t$pos\t$len]\n" if ($verbose);
+	    print "[$pos\t$len]\n" if ($verbose);
+	    
+	    $file =~ s/\.xml$//;
+	    print INX $file,"\t",b64_encode($pos),"\t",b64_encode($len),"\n";
+#	$inx{$kapv} = [$pos,$len];
 	
-	$inx{$kapv} = [$pos,$len];
-	
-   
-	
-	
-	$pos += $len;
+	    $pos += $len;
+	}
     }
-}
-
-closedir DIR;
+    closedir DIR;
 
 # skribu indekson
-open INX, ">$inxfile" or die "Ne povis malfermi $inxfile: $!\n";
+#open INX, ">$inxfile" or die "Ne povis malfermi $inxfile: $!\n";
 
-foreach $kapv (sort keys %inx) {
-    ($pos,$len) = @{$inx{$kapv}};
-    print INX "$kapv\t".b64_encode($pos)."\t".b64_encode($len)."\n";
+#foreach $kapv (sort keys %inx) {
+#    ($pos,$len) = @{$inx{$kapv}};
+#    print INX "$kapv\t".b64_encode($pos)."\t".b64_encode($len)."\n";
+#}
+
+    close INX;
+}
+
+
+########### 2a pasho: analizu indekso.xml kaj ekstraktu chiujn
+###########           informojn por la diverslingvaj indeksoj
+###########           kaj fine skribu tiujn
+
+# relegu la pozicio-indekson
+open INX,"$inxpref.inx" or die "Ne eblis legi \"$inxpref.inx\"\n";
+while (<INX>) {
+    chomp;
+    @entry=split("\t");
+    $positions{$entry[0]}=[$entry[1],$entry[2]];
 }
 close INX;
 
+# enlegu indekso.xml
+$/ = '</art';    
+open INX, "$indekso" or die "Ne eblis legi la dosieron \"$indekso\"\n";
+print "Analizas $indekso...\n" if ($verbose);
+while (<INX>) {
+    artikolo($_);
+}     
+close INX;
+$/ = "\n"; 
+
+# skribu lingvoindeksojn
+foreach $lng (keys %tradukoj) {
+    my $last0, $last1;
+
+    print "$lng...\n" if ($verbose);
+    $refs = $tradukoj{$lng};
+
+    open INX, ">$inxpref.$lng.inx";
+    $refs = $tradukoj{$lng};
+#    @$refs = sort { $a->[0] cmp $b->[0] } @$refs;
+    for $entry (sort { compare($a->[0],$b->[0]) } @$refs) {
+	if (($pos = $positions{$entry->[1]})
+	    and (($last0 ne $entry->[0]) or ($last1 ne $entry->[1]))) { 
+	    print INX $entry->[0], "\t", $pos->[0], "\t", $pos->[1], "\n";
+	    $last0 = $entry->[0];
+	    $last1 = $entry->[1];
+	}
+    }
+    close INX;
+}      
+
+
+########### helpfunkcioj
+###########
 ###########
 
 
@@ -111,3 +170,87 @@ sub int_utf8 {
         $c < 0x800 ? chr($c >>6&0x3F|0xC0) . chr($c & 0x3F | 0x80) :
             chr($c >>12&0x0F|0xE0).chr($c >>6&0x3F|0x80).chr($c &0x3F|0x80);
 } 
+
+# kompari konsiderante nur [A-Za-z0-9 ]
+sub compare {
+    $x = shift;
+    $y = shift;
+ 
+    $x =~ s/[^a-zA-Z0-9 ]//g;
+    $y =~ s/[^a-zA-Z0-9 ]//g;
+ 
+    $x = lc($x);
+    $y = lc($y);
+ 
+    return $x cmp $y;
+}               
+
+# analizas la indeks-tekston de artikolo
+ 
+sub artikolo {
+    my $tekst = shift;
+    my ($mrk,$kap,$rad,$first_lit,$last_lit);
+ 
+    # elprenu la markon
+    $tekst =~ s/^.*?<art\s+mrk="([^\"]*)"\s*>//s;
+    $mrk = $1;
+    unless ($mrk) {
+        # se tio ne estas la vosto de la dosiero, plendu
+        if ($tekst =~ /<\/art$/) {
+            warn "marko ne trovighis en $tekst\n";
+        }
+        return;
+    }
+
+    # trovu chiujn kapvortojn (inkl. de derivajhoj)
+    $tekst =~ s/<kap\s*>(.*?)<\/kap\s*>/
+	traduko($1,'eo',$mrk)/segx;
+
+    # trovu chiujn tradukojn
+    $tekst =~ s/<trd\s+lng="([^\"]*)"\s*>(.*?)<\/trd\s*>/
+	traduko($2,$1,$mrk)/segx; 
+
+    return '';
+}
+
+# notas unuopan tradukon
+
+sub traduko {
+    my ($trd,$lng,$mrk)=@_;
+    my $ind;
+
+    # forigu eblajn klr-ojn kaj aliajn ghenajn signojn
+    $trd =~ s/<klr[^>]>.*?<\/klr>//sg;
+    $trd =~ s/\///sg;
+
+    # kio estas la indeksenda vorto?
+    if ($trd =~ /<ind>(.*?)<\/ind>/s) {
+        $ind = $1;
+    } else {
+        $ind = $trd;
+    }
+ 
+    # forigu komencajn/finajn spacojn
+    $ind=~s/\s*$//s;
+    $ind=~s/^\s*//s;
+ 
+    # metu la vorton en la indekson
+    push @{$tradukoj{$lng}}, [$ind,$mrk];
+ 
+    return '';
+};          
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
