b840ee4ce8c66e4d7a13edb116685a0e1e9c51d2
revo <revo@5b0f4965-bd4a-0410-a764-d5e1455f809a>
2002-08-27 19:31:15 +0000

    uzu xsltproc anstatau xt
    git-svn-id: https://retavortaro.svn.sourceforge.net/svnroot/retavortaro/trunk@226 5b0f4965-bd4a-0410-a764-d5e1455f809a

--- bin/xslt.sh
+++ bin/xslt.sh
@@ -1,5 +1,10 @@
-CLASSPATH=/usr/lib/xml/xp.jar:/usr/lib/xml/xt.jar:/usr/lib/xml/sax.jar
-export CLASSPATH
+#old xt: CLASSPATH=/usr/lib/xml/xp.jar:/usr/lib/xml/xt.jar:/usr/lib/xml/sax.jar
 
-/usr/lib/java/bin/java com.jclark.xsl.sax.Driver $1 $2
+#XTPATH=/home/revo/xt-20020426a-src
+#new xt: CLASSPATH=$XTPATH/xt.jar:$XTPATH/lib/xp.jar:$XTPATH/lib/xml-apis.jar
+#export CLASSPATH
+
+#/usr/lib/java/bin/java com.jclark.xsl.sax.Driver $1 $2
+
+/usr/bin/xsltproc $2 $1
 
