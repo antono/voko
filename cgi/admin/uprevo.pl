3b6b7b122fee55a72a479b1b491b61941e04e9c6
wielandp <wielandp@5b0f4965-bd4a-0410-a764-d5e1455f809a>
2009-09-04 16:26:43 +0000

    Malpli parolema kaj pli log al dosiero.
    git-svn-id: https://retavortaro.svn.sourceforge.net/svnroot/retavortaro/trunk@443 5b0f4965-bd4a-0410-a764-d5e1455f809a

--- cgi/admin/uprevo.pl
+++ cgi/admin/uprevo.pl
@@ -27,7 +27,7 @@ my $homedir = "/var/www/web277";
 #print h1("homedir = $homedir");
 
 $ret = `du -sh $homedir`;
-print h2("du -> $exitcode");
+print LOG "du -> $exitcode\n$ret\n";
 print pre($ret);
 
 my $htmldir = "$homedir/html";
@@ -48,7 +48,7 @@ unless ($fname =~ /^revo-\d\d\d\d\d\d\d\d\.tgz$/) {
 
 my $dbfname = $fname;
 $dbfname =~ s/^revo-(.*).tgz$/revodb-$1.sql.gz/;
-print h2("dbfname -> $dbfname");
+print LOG "dbfname -> $dbfname\n";
 
 my $ret;
 
@@ -70,7 +70,7 @@ chdir $htmldir or die "chdir ne funkciis";
 
 $ret = `rm bv_forigu_tiujn.lst 2>&1`;
 $exitcode = $?;
-print h2("rm bv_forigu_tiujn.lst -> $exitcode");
+print LOG "rm bv_forigu_tiujn.lst -> $exitcode\n";
 
 $ret = `tar -xvzf alveno/$fname revo/art tgz revo/xml revo/xsl revo/cfg revo/tez revo/bld revo/stl revo/smb revo/dok revo/inx revo/index.html revo/sercxo.html revo/titolo.html revo/revo.ico revo/araneo.gif revo/reto.gif revo/revo.jpg revo/revo.gif revo/travidebla.gif bv_forigu_tiujn.lst 2>&1`;
 $exitcode = $?;
@@ -81,9 +81,10 @@ revorss::write($ret, $htmldir, -1, 0);
 
 # Connect to the database.
 my $dbh = parseart::connect();
-chdir $revodir or die "chdir revo ne funkciis";
+#chdir $revodir or die "chdir revo ne funkciis";
+chdir $xmldir or die "chdir revo ne funkciis";
 while ($ret =~ m/revo\/xml\/([^.]+)\.xml/gm) {
-  print pre("- $1 -")."\n";
+#  print pre("- $1 -")."\n";
   parseart::parse($dbh, $1, $xmldir, 0);
   parseart2::parse($dbh, $1, $xmldir, 0);
 }
@@ -93,15 +94,15 @@ chdir $htmldir or die "chdir html ne funkciis";
 ########### forigi ##############
 $ret = `ls -l bv_forigu_tiujn.lst 2>&1`;
 $exitcode = $?;
-print h2("ls -> $exitcode");
+#print h2("ls -> $exitcode");
 print LOG "forigi: ls -> $exitcode\n$ret";
-print pre($ret);
+#print pre($ret);
 
 $ret = `pwd 2>&1`;
 $exitcode = $?;
-print h2("pwd -> $exitcode");
+#print h2("pwd -> $exitcode");
 print LOG "pwd -> $exitcode\n$ret";
-print pre($ret);
+#print pre($ret);
 
 if (open IN, "<bv_forigu_tiujn.lst") {
 #  print h2("open true");
@@ -128,16 +129,16 @@ if (open IN, "<bv_forigu_tiujn.lst") {
 
 $ret = `cat bv_forigu_tiujn.lst 2>&1`;
 $exitcode = $?;
-print h2("cat -> $exitcode");
+#print h2("cat -> $exitcode");
 print LOG "cat -> $exitcode\n$ret";
 print pre($ret);
 
-print h2("date: ".`date`);
+print LOG "date: ".`date`."\n";
 
 my $dumpcmd = revodb::mysqldump;
 $ret = `$dumpcmd --skip-lock-tables | gzip >$htmldir/alveno/$dbfname`;
-print pre("mysqldump -> \n$ret"),
-      h2("date: ".`date`);
+print LOG "mysqldump -> \n$ret",
+      "date: ".`date`;
 
 if (0 and !$exitcode) {
   $ret = `rm alveno/$fname 2>&1`;
@@ -153,20 +154,20 @@ if (0 and !$exitcode) {
 }
 
 $ret = `du -sh $homedir`;
-print h2("du -> $exitcode");
+#print h2("du -> $exitcode");
 print pre($ret);
 
 my $findargs = "$htmldir/alveno -mtime +7 -name \\*gz";
 $ret = `find $findargs`;
-print pre("find $findargs -> \n$ret"),
+print LOG "find $findargs -> \n$ret\n";
 $ret = `find $findargs | xargs rm`;
-print pre("find rm -> \n$ret"),
+print LOG "find rm -> \n$ret\n";
 
 $findargs = "$htmldir/alveno -mtime +2 -name revodb\\*gz";
 $ret = `find $findargs`;
-print pre("find $findargs -> \n$ret"),
+print LOG "find $findargs -> \n$ret\n",
 $ret = `find $findargs | xargs rm`;
-print pre("find rm -> \n$ret"),
+print LOG "find rm -> \n$ret\n";
 
 my $fsize = `du -h $htmldir/alveno/$fname`; $fsize =~ s/\t.*$//; chomp $fsize;
 my $dbsize = `du -h $htmldir/alveno/$dbfname`; $dbsize =~ s/\t.*$//; chomp $dbsize;
