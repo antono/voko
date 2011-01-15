7924c64cf52e6313bac521f174b4bf0d43871fd9
wdiestel <wdiestel@5b0f4965-bd4a-0410-a764-d5e1455f809a>
2011-01-15 15:40:59 +0000

    adaptis purigon
    git-svn-id: https://retavortaro.svn.sourceforge.net/svnroot/retavortaro/trunk@516 5b0f4965-bd4a-0410-a764-d5e1455f809a

--- bin/redaktoservo.pl
+++ bin/redaktoservo.pl
@@ -11,26 +11,31 @@ if ($ARGV[0] eq '-r') {
   open VF, "$command|" or die "Ne povas dukti de ant: $!\n";
   while (<VF>) { print; };
   close VF;
+  exit;
+}   
 
-} else {  
 
-  if ($ARGV[0] eq '-a') {
+if ($ARGV[0] eq '-a') {
     $target = "srv-servo-art";
-  } else {
+  
+} elsif ($ARGV[0] eq '-p') {
+    $target = "srv-purigu";
+
+} else {
     $target = "-Duser-mail-file-exists=true srv-servo"; # nepre rekreu la vortaron eĉ se tiufoje nealvenis poŝto  
     # $target = "srv-servo";
-  } 
+} 
 
-  $datetime = `date +%Y%m%d_%H%M%S`;
-  chomp($datetime);
+$datetime = `date +%Y%m%d_%H%M%S`;
+chomp($datetime);
 
 #  chdir $ENV{"REVO"};
-  $log = $ENV{"HOME"}."/private/revolog/redsrv-$datetime.log";
+$log = $ENV{"HOME"}."/private/revolog/redsrv-$datetime.log";
 
-  $command="ant -file $ENV{\"VOKO\"}/ant/redaktoservo.xml $target 2>&1 | tee $log";
-  print "$command\nTIME:",`date`,"\n" if ($verbose);
+$command="ant -file $ENV{\"VOKO\"}/ant/redaktoservo.xml $target 2>&1 | tee $log";
+print "$command\nTIME:",`date`,"\n" if ($verbose);
 
-  open VF, "$command|" or die "Ne povas dukti de ant: $!\n";
-  while (<VF>) { print; }; 
-  close VF;
-}
+open VF, "$command|" or die "Ne povas dukti de ant: $!\n";
+while (<VF>) { print; }; 
+close VF;
+
